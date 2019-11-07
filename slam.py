a8199f286e928b204e00cbe9ab9da45fee8f4e4f
Parth Chadha <parth29@gmail.com>
2019-11-07 08:05:35 -0800

    WIP pangloin works

--- slam.py
+++ slam.py
@@ -1,30 +1,132 @@
 #!/usr/bin/env python3
 import cv2
 from display import Display
-from extractor import Extractor
+from frame import Frame, match_frames, denormalize, IRt
 import numpy as np
-
-W = 1920//2
-H = 1080//2
-display = Display(W,H)
-F = 1
+import g2o
+import OpenGL.GL as gl
+import pangolin
+        
+# camera intrensics 
+W, H = 1920//2, 1080//2
+F = 270
 K = np.array(([F,0,W//2],[0,F,H//2],[0,0,1]))
+Kinv = np.linalg.inv(K)
 
-fe = Extractor(K)#W, H)
+from multiprocessing import Process, Queue
+
+class Map(object):
+    def __init__(self):
+        self.frames = []
+        self.points = []
+        self.state = None
+        self.q = Queue()
+        p = Process(target=self.viewer_thread, args=(self.q,))
+        p.daemon = True
+        p.start()
+
+    def viewer_thread(self, q):
+        self.viewer_init()
+        while 1:
+            self.viewer_refresh(q)
+        
+    def viewer_init(self):
+        
+        pangolin.CreateWindowAndBind('Main', 640, 480)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        self.scam = pangolin.OpenGlRenderState(
+            pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+            pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+        self.handler = pangolin.Handler3D(self.scam)
+
+        # Create Interactive View in window
+        self.dcam = pangolin.CreateDisplay()
+        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+        self.dcam.SetHandler(self.handler)
+
+    def viewer_refresh(self, q):
+        if self.state is None or not q.empty():
+            self.state = q.get()
+
+        ppts = np.array([d[:3,3] for d in self.state[0]])
+        spts = np.array(self.state[1])
+        
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+
+        gl.glPointSize(10)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(ppts)
+
+        gl.glPointSize(2)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(spts)
+
+        pangolin.FinishFrame()
+        
+    def display(self):
+        poses, pts = [], []
+        for f in self.frames:
+            poses.append(f.pose)
+        for p in self.points:
+            pts.append(p.pt)
+        self.q.put((poses, pts))
+
+# main classes
+#display = Display(W,H)
+mapp = Map()
+        
+class Point(object):
+    # A Point is a 3-D point in the world
+    # Each Point is observed in multiple Frames    
+    def __init__(self, mapp, loc):
+        self.pt = loc
+        self.frames = []
+        self.idxs = []
+        self.id = len(mapp.points)
+        mapp.points.append(self)
+        
+    def add_observation(self, frame, idx):
+        self.frames.append(frame)
+        self.idxs.append(idx)
+
+def triangulate(pose1, pose2, pts1, pts2):
+    return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+
 
 def process_frame(img):
     img = cv2.resize(img, (W,H))
-    matches = fe.extract(img)
-    if matches is None:
+    frame = Frame(mapp, img, K)
+    if frame.id == 0:
         return
+    
+    f1 = mapp.frames[-1]
+    f2 = mapp.frames[-2]
+    idx1, idx2, Rt  = match_frames(f1, f2)
+    f1.pose = np.dot(Rt, f2.pose)
 
-    for pt1, pt2 in matches:
-        u1, v1 = fe.denormalize(pt1)#map(lambda x : int(round(x)), pt1)
-        u2, v2 = fe.denormalize(pt2)#map(lambda x : int(round(x)), pt2)
+    pts4d = triangulate(f1.pose, f2.pose, f1.pts[idx1], f2.pts[idx2])
+    pts4d /= pts4d[:,3:]
+    
+    good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:,2] > 0)
+    pts4d = pts4d[good_pts4d]
+
+    for i, p in enumerate(pts4d):
+        if not good_pts4d[i]:
+            continue
+        pt = Point(mapp, p)
+        pt.add_observation(f1, idx1[i])
+        pt.add_observation(f2, idx2[i])
+    
+    for pt1, pt2 in zip(f1.pts[idx1], f2.pts[idx2]):
+        u1, v1 = denormalize(K, pt1)
+        u2, v2 = denormalize(K, pt2)
         cv2.circle(img, (u1,v1), color=(0,255,0), radius=3)
         cv2.line(img, (u1,v1), (u2,v2), color=(255,0,0))
 
-    display.paint(img)
+    #display.paint(img)
+    mapp.display()
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("test_nyc.mp4")
